994cb42a181d9e420cdbb8f959b0081d71fc2f30
spenserkuroda <56206258+snkuroda@users.noreply.github.com>
2023-03-08 20:03:56 -0800

    initialize map

---
+++ app.js
@@ -0,0 +1,6 @@
+var mymap = L.map('map').setView([44.000000,-120.500000], 7);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(mymap);
